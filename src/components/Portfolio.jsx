0804ea36dad7c12c7f08866e4156c9bd9dc8168c
elijahmandeville <elijahmandeville@gmail.com>
2022-06-23 16:06:41 -0600

    adding code for rendering page below terminal

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Portfolio() {
+  return (
+    <>
+      <h1>Portfolio here</h1>
+      <p>This is my dope portfolio</p>
+    </>
+  )
+}
+
+export default Portfolio
--- src/components/TerminalWindow.jsx
+++ src/components/TerminalWindow.jsx
@@ -2,78 +2,74 @@ import React, { Component } from "react";
 import "../styles/TerminalWindow.css";
 import { motion } from 'framer-motion';
 import Terminal from "terminal-in-react";
+import Portfolio from "./Portfolio";
 
 class TerminalWindow extends Component {
   constructor(props) {
     super(props)
     this.transition = { duration: .6, ease: [0.43, 0.13, 0.23, 0.96]};
+    this.currentPage = "portfolio";
+    this.renderPage = this.renderPage.bind(this);
+  }
+
+  renderPage(page = this.currentPage) {
+    //Add functionality for rendering page below terminal based on command. Alternative would be a new page with a back to terminal button.
+      if(page == "") {
+        return
+      }
+      if(page == "portfolio") {
+        return <Portfolio />
+      }
   }
 
   render(){
     return (
-      // <motion.div 
-      //   exit={{ scale: 0 }} 
-      //   initial={{ scale: 0 }} 
-      //   animate={{ scale: "100%" }}
-      //   transition={transition}
-      //   className='project-list'>
-      //   <Terminal
-      //     color='green'
-      //     backgroundColor='black'
-      //     barColor='black'
-      //     style={{ fontWeight: "bold", fontSize: "1em" }}
-      //     commands={{
-      //       'open-google': () => window.open('https://www.google.com/', '_blank'),
-      //       showmsg: this.showMsg,
-      //       popup: () => alert('Terminal in React')
-      //     }}
-      //     descriptions={{
-      //       'open-google': 'opens google.com',
-      //       showmsg: 'shows a message',
-      //       alert: 'alert', popup: 'alert'
-      //     }}
-      //     msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
-      //   />
-      //   <a href="/" className="fa-regular fa-hand-spock fa-4x"></a>
-      // </motion.div>
-    
-    <motion.div
-      exit={{ scale: 0 }} 
-      initial={{ scale: 0 }} 
-      animate={{ scale: "100%" }}
-      transition={this.transition}
-      className='project-list'
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh"
-      }}
-    >
-      <a href="/" className="fa-regular fa-hand-spock fa-4x"></a>
-      <Terminal
-        color='orange'
-        backgroundColor='black'
-        barColor='black'
-        prompt='orange'
-        allowTabs={false}
-        hideTopBar={false}
-        startState='maximised'
-        style={{ fontWeight: "none", fontSize: "1.5em" }}
-        commands={{
-          'open-google': () => window.open('https://www.google.com/', '_blank'),
-          showmsg: this.showMsg,
-          popup: () => alert('Terminal in React')
-        }}
-        descriptions={{
-          'open-google': 'opens google.com',
-          showmsg: 'shows a message',
-          alert: 'alert', popup: 'alert'
-        }}
-        msg="Welcome to my portfolio site! My name is Elijah Mandeville, and I'm a full-stack web developer. Type 'help' to get started!"
-      />
-      
-    </motion.div>
+      <>
+        <motion.div
+          exit={{ scale: 0 }} 
+          initial={{ scale: 0 }} 
+          animate={{ scale: "100%" }}
+          transition={this.transition}
+          className='project-list'
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh"
+          }}
+        >
+          <a href="/" className="fa-regular fa-hand-spock fa-4x"></a>
+          <Terminal
+            color='orange'
+            backgroundColor='black'
+            barColor='black'
+            prompt='orange'
+            allowTabs={false}
+            hideTopBar={false}
+            startState='maximised'
+            style={{ fontWeight: "none", fontSize: "1.5em" }}
+            commands={{
+              'open-google': () => window.open('https://www.google.com/', '_blank'),
+              'showmsg': this.showMsg,
+              'popup': () => alert('Terminal in React'),
+              'portfolio': () => {
+                this.setState(this.currentPage, "portfolio")
+              }
+            }}
+            descriptions={{
+              'open-google': 'opens google.com',
+              'showmsg': 'shows a message',
+              'alert': 'alert', popup: 'alert',
+              'portfolio': `page updated to ${this.currentPage}`
+              
+            }}
+            msg="Welcome to my portfolio site! My name is Elijah Mandeville, and I'm a full-stack web developer. Type 'help' to get started!"
+          />
+
+          
+        </motion.div>
+        {this.renderPage}
+    </>
     );
   }
 }
